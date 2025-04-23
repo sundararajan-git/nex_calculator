9684efe90b814888f02ea4e0034eabf291852f28
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-04-23 06:57:34 +0530

    re direct home

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,17 @@
-import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const RootLayout = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const cleanPath = location.pathname.replace(/([^:]\/)\/+/g, "$1");
+    console.log(cleanPath);
+    if (location.pathname !== cleanPath) {
+      navigate(cleanPath, { replace: true }); // Replace URL without adding to history
+    }
+  }, [location, navigate]);
   return (
     <section className="h-dvh overflow-auto">
       <NavBar />
