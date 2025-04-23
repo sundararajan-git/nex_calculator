8ee6ccf168e8b2e82ce395ccc7d87e4be3e764d5
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-04-23 06:30:26 +0530

    try again

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { HashRouter } from "react-router-dom";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
